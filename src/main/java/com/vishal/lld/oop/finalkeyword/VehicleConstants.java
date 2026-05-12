71b1e810cbe81c2ca4780ac20b681523e4bb984a
Vishal Sharma <vishalimp03@gmail.com>
2026-05-12 17:58:17 +0530

    fix: Add missing SuppressWarnings annotation for unused MAX_SPEED constant

--- src/main/java/com/vishal/lld/oop/finalkeyword/VehicleConstants.java
+++ src/main/java/com/vishal/lld/oop/finalkeyword/VehicleConstants.java
@@ -3,5 +3,6 @@ package com.vishal.lld.oop.finalkeyword;
 public final class VehicleConstants {
 
     // final class cannot be en=xtensible
+    @SuppressWarnings("unused")
     private final static Integer MAX_SPEED = 300;
 }
